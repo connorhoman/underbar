f1d971cd8290fca90c7a26b7127bd3eee85972eb
Connor Homan <connorhoman14@gmail.com>
2019-03-11 16:10:31 -0700

    Added Filter and Reject

--- src/underbar.js
+++ src/underbar.js
@@ -77,16 +77,33 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+  	var result = [];
+  	_.each(collection, function(item) {
+  		if (test(item)) {
+  			result.push(item);
+  		}
+  	});
+
+  	return result;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var result = [];
+  	_.each(collection, function(item) {
+  		if (!test(item)) {
+  			result.push(item);
+  		}
+  	});
+
+  	return result;
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
+  	
   };
 
 
